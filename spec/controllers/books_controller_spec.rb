885d613c7d4aefb569d7c6ae09683b4448a6747a
James Lee <junghlee90@gmail.com>
2015-01-28 15:42:14 -0600

    test!

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe BooksController, :type => :controller do
-
+describe BooksController, :type => :controller do
+  describe "#index" do
+    it "returns all books" do
+      FactoryGirl.create(:book)
+      get :index
+      expect(JSON.parse(response.body).length).to eq(1)
+    end
+  end
 end
